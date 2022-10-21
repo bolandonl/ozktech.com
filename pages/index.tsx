61201df72140646d4c675f9994134173318ed71c
Michael Boland <michael@boland.onl>
2022-10-21 02:33:40 -0500

    Add one-liners.

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ function Home() {
                       Name
                     </th>
                     <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
-                      Description
+                      One-liner
                     </th>
                     <th className="px-6 py-3 bg-gray-100 text-left text-xs leading-4 font-medium text-gray-600 uppercase tracking-wider">
                       Type
