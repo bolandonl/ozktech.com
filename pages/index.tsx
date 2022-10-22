d5523cd9173af03459349d933d38c34f28dad910
Michael Boland <michael@boland.onl>
2022-10-21 23:00:57 -0500

    Update about page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ function Home() {
         <title>ozktech.com</title>
       </Head>
       <DefaultSeo {...SEO} />
-      <div className="flex flex-col mb-12">
+      <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
